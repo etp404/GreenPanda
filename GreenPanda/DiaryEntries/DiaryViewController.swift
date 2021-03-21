34abdd1d85e6a6bb89334b1b2318546c964dd88c
Matt Mould <etp404@users.noreply.github.com>
2021-03-21 10:46:56 +0000

    Checkpoint

--- GreenPanda/DiaryEntries/DiaryViewController.swift
+++ GreenPanda/DiaryEntries/DiaryViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import Combine
+import Charts
 class DiaryViewController: ViewController {
 
     private var someCancellable: AnyCancellable?
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var chart: LineChartView!
     @IBAction func composeButtonPRessed(_ sender: Any) {
         viewModel?.composeButtonPressed()
     }
@@ -43,6 +45,8 @@ class DiaryViewController: ViewController {
         someCancellable = viewModel?.$entries.sink{entries in
             self.applySnapshot(entries:entries)
         }
+        
+        doGraph()
 
     }
     
@@ -52,6 +56,36 @@ class DiaryViewController: ViewController {
         snapshot.appendSections([0])
         snapshot.appendItems(entries)
         dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+    
+    func doGraph() {
+//        let ys1 = [0.0, 5.0, 3.0, 2.0, 5.0, 5.0, 3.0, 2.0, 1.0]
+//
+//        let yse1 = ys1.enumerated().map { x, y in return ChartDataEntry(x: x, y: y) }
+        
+        let now = Date()
+        let yse1 = Array(0...10).reversed().map({(index: Int) -> ChartDataEntry in
+            let x = now.timeIntervalSince1970 - Double(index*24*60*60)
+            return ChartDataEntry(x:x , y: Double(Array(1...5).randomElement()!))
+        })
+        
+        let data = LineChartData()
+        let ds1 = LineChartDataSet(entries: yse1)
+        ds1.drawFilledEnabled = true
+        ds1.drawCirclesEnabled = true
+        ds1.mode = .cubicBezier
+        data.append(ds1)
+        data.setDrawValues(false)
+
+        self.chart.data = data
+        
+        chart.leftAxis.enabled = false
+        chart.rightAxis.enabled = false
+        chart.legend.enabled = false
+        chart.xAxis.drawGridLinesEnabled = false
+        chart.xAxis.labelPosition = XAxis.LabelPosition.bottom
+
+
     }
 
 }
